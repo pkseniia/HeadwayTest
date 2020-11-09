3a4bd9077a3f48c35dfe27174754e82ede0f8d1e
Kseniia Poternak <ksu93ha@gmail.com>
2020-11-09 18:07:44 +0200

    Initial commit

---
+++ HeadwayTest/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  HeadwayTest
+//
+//  Created by Kseniia Poternak on 09.11.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
